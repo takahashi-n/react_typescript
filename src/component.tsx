85f63691d046a7c211796a98cad6f84fce5699ac
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-05-11 16:21:08 +0900

    useMemo

--- src/component.tsx
+++ src/component.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import { useState, useCallback } from "react"
+import React, { useState, useCallback, useMemo } from "react"
 
 const Component: React.FC = () => {
-  const [count, setCount] = useState(0)
-  const handleClick = useCallback(() => {
-    setCount(prev => prev + 1)
+  const [count, setCount] = useState<number | null>(0)
+  const double = useMemo<number | null>(() => {
+    if (count === null) return null
+    return count * 2
   }, [count])
+  const doubleWithUnit = useMemo<string | null>(() => {
+    if (double === null) return null
+    return `${double} pt`
+  }, [double])
+  const handleClick = useCallback(() => {
+    setCount(prev => {
+      if (prev === null) return 0
+      return prev + 1
+    })
+  }, [])
   return (
     <div>
-      <p>{count}</p>
+      <p>count : {count}</p>
+      <p>double : {double}</p>
+      <p>doubleWithUnit : {doubleWithUnit}</p>        
       <button onClick={handleClick}>+1</button>
     </div>
   )
