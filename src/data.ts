53535a3aa0baf1e5ecc7eb4418b16ec31429f256
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-05-11 15:00:29 +0900

    リファクタリング

--- src/data.ts
+++ src/data.ts
@@ -1,19 +1,24 @@
+export type Row = {
+  id: string,
+  age: string,
+  answers: (number | null)[]
+}
+export type Rows = Row[]
+
 export const rows: Rows = [
   {
     id: 'up20-up30',
-    generation: '20～30歳',
+    age: '20～30歳',
     answers: [0.18, 0.22, 0.37, 0.23]
   },
   {
     id: 'up30-up40',
-    generation: '30～40歳',
+    age: '30～40歳',
     answers: [0.13, 0.27, 0.42, 0.18]
-  }  
+  },
+  {
+    id: 'up50-up60',
+    age: '50～60歳',
+    answers: [null, null, null, null]
+  }
 ]
-
-export type Row = {
-  id: string,
-  generation: string,
-  answers: number[]
-}
-export type Rows = Row[]
